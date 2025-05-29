ef40e66e55cedeace68f5d434f97468718c5d352
Thibault Pouch <thibault.pouch@epitech.eu>
2025-05-29 14:57:47 +0200

    [Chore] Map and Resource classes: enhance documentation for clarity,

--- Resource.hpp
+++ Resource.hpp
@@ -3,31 +3,69 @@
 #include <raylib.h>
 #include <string>
 
+/**
+ * @enum ResourceType
+ * @brief Types of resources available in the game
+ */
 enum class ResourceType {
-    FOOD = 0,
-    LINEMATE = 1,
-    DERAUMERE = 2,
-    SIBUR = 3,
-    MENDIANE = 4,
-    PHIRAS = 5,
-    THYSTAME = 6
+    FOOD = 0,      ///< Food resource
+    LINEMATE = 1,  ///< Linemate resource
+    DERAUMERE = 2, ///< Deraumere resource
+    SIBUR = 3,     ///< Sibur resource
+    MENDIANE = 4,  ///< Mendiane resource
+    PHIRAS = 5,    ///< Phiras resource
+    THYSTAME = 6   ///< Thystame resource
 };
 
+/**
+ * @class Resource
+ * @brief Represents a resource item in the game world
+ * 
+ * This class handles resource rendering and properties.
+ */
 class Resource {
 private:
-    ResourceType type;
-    Vector3 position;
-    Color color;
-    std::string name;
+    ResourceType type;    ///< Type of the resource
+    Vector3 position;     ///< Position in 3D space
+    Color color;          ///< Color representation
+    std::string name;     ///< Resource name
 
 public:
+    /**
+     * @brief Constructor for Resource
+     * @param resType Type of the resource
+     * @param pos Position in the world
+     */
     Resource(ResourceType resType, Vector3 pos);
+    
+    /**
+     * @brief Destructor for Resource
+     */
     ~Resource();
 
+    /**
+     * @brief Draws the resource in the world
+     * @param worldPos Base position in the world
+     * @param tileSize Size of a map tile
+     */
     void draw(Vector3 worldPos, int tileSize) const;
 
+    /**
+     * @brief Gets the resource type
+     * @return The resource type
+     */
     ResourceType getType() const;
+    
+    /**
+     * @brief Gets the resource position
+     * @return The position vector
+     */
     Vector3 getPosition() const;
+    
+    /**
+     * @brief Gets the resource color
+     * @return The color
+     */
     Color getColor() const;
     std::string getName() const;
 
