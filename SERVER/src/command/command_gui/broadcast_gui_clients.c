b2976f77f80c98efe29f2d792d7a3567c8b04655
charles.raissiguier <charles.raissiguier@epitech.eu>
2025-06-29 21:37:57 +0200

    fix: music function warning

--- SERVER/src/command/command_gui/broadcast_gui_clients.c
+++ SERVER/src/command/command_gui/broadcast_gui_clients.c
@@ -52,32 +52,6 @@ void broadcast_to_ai_clients(server_t *server, const char *message)
     }
 }
 
-static void send_message_to_player(server_t *server, int sender_id,
-    int receiver_id, const char *text)
-{
-    char buffer[512];
-    int direction;
-
-    direction = calculate_sound_direction(server,
-        &server->players[sender_id], &server->players[receiver_id]);
-    snprintf(buffer, sizeof(buffer), "message %d, %s\n", direction, text);
-    send(server->players[receiver_id].socket, buffer, strlen(buffer), 0);
-}
-
-void handle_player_broadcast(server_t *server, int sender_id,
-    const char *text)
-{
-    int i;
-
-    for (i = 0; i < server->num_players; i++) {
-        if (i != sender_id && server->players[i].team_id != -1 &&
-            server->players[i].socket > 0) {
-            send_message_to_player(server, sender_id, i, text);
-        }
-    }
-    send_gui_pbc(server, sender_id, text);
-}
-
 static int get_wrapped_distance(int coord1, int coord2, int max_size)
 {
     int direct_dist;
@@ -105,4 +79,30 @@ int calculate_sound_direction(server_t *server, player_t *sender,
     angle = atan2(dy, dx);
     direction = ((int)(angle * 4 / (2 * M_PI)) + receiver->orientation) % 8;
     return direction + 1;
+}
+
+static void send_message_to_player(server_t *server, int sender_id,
+    int receiver_id, const char *text)
+{
+    char buffer[512];
+    int direction;
+
+    direction = calculate_sound_direction(server,
+        &server->players[sender_id], &server->players[receiver_id]);
+    snprintf(buffer, sizeof(buffer), "message %d, %s\n", direction, text);
+    send(server->players[receiver_id].socket, buffer, strlen(buffer), 0);
+}
+
+void handle_player_broadcast(server_t *server, int sender_id,
+    const char *text)
+{
+    int i;
+
+    for (i = 0; i < server->num_players; i++) {
+        if (i != sender_id && server->players[i].team_id != -1 &&
+            server->players[i].socket > 0) {
+            send_message_to_player(server, sender_id, i, text);
+        }
+    }
+    send_gui_pbc(server, sender_id, text);
 }
